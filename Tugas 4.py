a8b9819a530c133cabd3e15f01da45580a82186c
jpr-exe <jseptian246@gmail.com>
2025-04-16 23:50:09 +0700

    tugas ai

--- Tugas 4.py
+++ Tugas 4.py
@@ -44,10 +44,23 @@ def solution_05():
     train_generator= ImageDataGenerator(rescale=1./255)
     
     # YOUR CODE HERE
+    validation_generator = train_generator.flow_from_directory(
+        TRAINING_DIR,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary'
+    )
 
     model=tf.keras.models.Sequential([
         # YOUR CODE HERE, end with a Neuron Dense, activated by sigmoid
-
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
                 tf.keras.layers.Dense(1, activation='sigmoid') #DO NOT CHANGE THIS LINE!
         ])
 
